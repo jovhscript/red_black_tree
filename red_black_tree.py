49635f8069b7a2cf8107461a3f2b65447393b101
Heidi Chen <s.heidi.chen@gmail.com>
2016-11-28 01:42:14 -0500

    fixed coloring to allow balancing

--- red_black_tree.py
+++ red_black_tree.py
@@ -63,10 +63,6 @@ class RedBlackNode(BinaryNode):
         self.right_ref = right_ref
         self.color = color
 
-    # def blacken(self):
-    #     if self.is_red():
-    #         self.color = Color.BLACK
-
     def is_black(self):
         return self.color == Color.BLACK
 
@@ -75,15 +71,6 @@ class RedBlackNode(BinaryNode):
 
     def is_empty(self):
         return False
-
-# class EmptyRedBlackNode(RedBlackNode):
-#     def __init__(self):
-#         self.color = Color.BLACK
-#         self.left_ref = RedBlackNodeRef()
-#         self.right_ref = RedBlackNodeRef()
-
-#     def is_empty(self):
-#         return True
 
 
 class RedBlackTree(BinaryTree):
@@ -100,16 +87,21 @@ class RedBlackTree(BinaryTree):
         #node = self._follow(ref)
         if node is None:
             return node
-        return RedBlackNode.from_node(node, color=Color.BLACK)
+        newnode = RedBlackNode.from_node(node, color=Color.BLACK)
+        return RedBlackNodeRef(newnode)
 
     def rotate_left(self, node):
         right = self._follow(node.right_ref)
-        newleft_right = RedBlackNode.from_node(self._follow(right.left_ref),
-                                               left_ref = RedBlackNodeRef(),
-                                               right_ref = RedBlackNodeRef(),
-                                               color = node.color)
+        if self._follow(right.left_ref) is not None:
+            newleft_right = RedBlackNode.from_node(self._follow(right.left_ref),
+                                                   left_ref = RedBlackNodeRef(),
+                                                   right_ref = RedBlackNodeRef(),
+                                                   color = node.color)
+            newleft_right_ref = RedBlackNodeRef(referent = newleft_right)
+        else:
+            newleft_right_ref = RedBlackNodeRef()
         newleft = RedBlackNode.from_node(node, 
-                                         right_ref = RedBlackNodeRef(referent=newleft_right))
+                                         right_ref = newleft_right_ref)
         newnode = RedBlackNode.from_node(right,
                                          left_ref = RedBlackNodeRef(referent=newleft))
         return newnode
@@ -117,13 +109,17 @@ class RedBlackTree(BinaryTree):
 
     def rotate_right(self, node):
         left = self._follow(node.left_ref)
-        newright_left = RedBlackNode.from_node(self._follow(left.right_ref),
-                                               left_ref = RedBlackNodeRef(),
-                                               right_ref = RedBlackNodeRef(),
-                                               color = node.color)
+        if self._follow(left.right_ref) is not None:
+            newright_left = RedBlackNode.from_node(self._follow(left.right_ref),
+                                                   left_ref = RedBlackNodeRef(),
+                                                   right_ref = RedBlackNodeRef(),
+                                                   color = node.color)
+            newright_left_ref = RedBlackNodeRef(referent = newleft_right)
+        else:
+            newright_left_ref = RedBlackNodeRef()
                                         
         newright = RedBlackNode.from_node(node, 
-                                          left_ref = RedBlackNodeRef(referent=newright_left))
+                                          left_ref = newright_left_ref)
         newnode = RedBlackNode.from_node(left,
                                          right_ref = RedBlackNodeRef(referent=newright))
 
@@ -134,8 +130,8 @@ class RedBlackTree(BinaryTree):
         right = self._blacken(self._follow(node.right_ref))
 
         return RedBlackNode.from_node(node, 
-                                      left_ref = RedBlackNodeRef(referent = left),
-                                      right_ref = RedBlackNodeRef(referent = right),
+                                      left_ref = left,
+                                      right_ref = right,
                                       color=Color.RED)
 
     def _isred(self, node):
@@ -145,7 +141,6 @@ class RedBlackTree(BinaryTree):
             return node.color == Color.RED
 
     def balance(self, node):
-        #node = self._follow(ref)
 
         if self._isred(node) | (node is None):
             return node
@@ -155,24 +150,22 @@ class RedBlackTree(BinaryTree):
 
         if self._isred(left):
             if self._isred(right):
-                newnode =  node
+                return self.recolor(node)
             if self._isred(self._follow(left.left_ref)):
-                newnode = self.rotate_right(node)
+                return self.recolor(self.rotate_right(node))
             if self._isred(self._follow(left.right_ref)):
                 newleft = self.rotate_left(left)
                 newnode = RedBlackNode.from_node(node, 
                                                  left_ref = RedBlackNodeRef(referent = newleft))
-                newnode = self.rotate_right(newnode)
-            return self.recolor(newnode)
+                return self.recolor(self.rotate_right(newnode))
         if self._isred(right):
             if self._isred(self._follow(right.right_ref)):
-                newnode = self.rotate_left(node)
+                return self.recolor(self.rotate_left(node))
             if self._isred(self._follow(right.left_ref)):
                 newright = self.rotate_right(right)
                 newnode = RedBlackNode.from_node(node, 
                                                  right_ref = RedBlackNodeRef(referent = newright))
-                newnode = self.rotate_left(newnode)
-            return self.recolor(newnode)
+                return self.recolor(self.rotate_left(newnode))
 
         return node
         
@@ -189,6 +182,7 @@ class RedBlackTree(BinaryTree):
         value_ref = ValueRef(value)
         #insert and get new tree ref
         self._tree_ref = self._insert(node, key, value_ref)
+        self._tree_ref = self._blacken(self._follow(self._tree_ref))
 
 
     def _insert(self, node, key, value_ref):
@@ -212,7 +206,7 @@ class RedBlackTree(BinaryTree):
                     right_ref=RedBlackNodeRef(referent=newright)))
         else: #create a new node to represent this data
             new_node = RefBlackNode.from_node(node, value_ref=value_ref)
-        new_node = self._blacken(new_node)
+        #new_node = self._blacken(new_node)
         return RedBlackNodeRef(referent=new_node)
 
 
